e0e20b3006653d9db341ee36c39a2dfd9124acef
walwalka <pbballow@gmail.com>
2023-10-10 12:58:54 -0400

    adding cors to the app

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import { PORT } from './config.js';
 import poolRoute from './routes/weatherRoute.js'
-
+import cors from 'cors';
 
 const app = express()
 
 app.use(express.json());
 
+// middleware for handling CORS policy
+app.use(cors());
+
+// Default route for app
 app.use('/', poolRoute);
 
+// weather api endpoint
 app.use('/weather', poolRoute);
 
 app.listen(PORT, () => {
